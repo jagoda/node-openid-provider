a3b5389bfc63d5d88d4853e07a5906d020d4dc1d
Chris Reid <chris@reidsy.com>
2013-08-27 11:35:07 +1000

    Initial import

---
+++ index.js
@@ -0,0 +1,44 @@
+var fs = require('fs');
+var express = require('express');
+//var templates = require('./templates');
+var openid = require('./openid.js').openid;
+
+var app = express();
+//app.use(express.logger());
+app.use(express.bodyParser());
+
+app.get('/id/:name', function(req, res) {
+	console.log(req.method, req.path);
+	fs.readFile('./static/user.xrds.xml', function(err, data) {
+		res.header('Content-Type', 'application/xrds+xml;charset=utf-8');
+		res.send(data);
+		res.end();
+	});
+});
+
+app.get('/yadis/:name', function(req, res) {
+	console.log(req.method, req.path);
+	fs.readFile('./static/user.xrds.xml', function(err, data) {
+		res.header('Content-Type', 'application/xrds+xml;charset=utf-8');
+		res.send(data);
+		res.end();
+	});
+});
+
+app.get('/*', function(req, res) {
+	console.log(req.method, req.path);
+	fs.readFile('./static/server.xrds.xml', function(err, data) {
+		res.header('Content-Type', 'application/xrds+xml;charset=utf-8');
+		res.send(data);
+		res.end();
+	});
+});
+
+app.post('/*', function(req, res) {
+	console.log(req.method, req.path);
+	res.send(openid(req, res));
+	res.end();
+})
+
+app.listen(3000);
+console.log("serving on port 3000");
---
+++ openid.js
@@ -0,0 +1,126 @@
+var crypto = require('crypto');
+var url = require('url');
+
+//constants
+var DH_MODULUS_HEX = "DCF93A0B883972EC0E19989AC5A2CE310E1D37717E8D9571BB7623731866E61EF75A2E27898B057F9891C2E27A639C3F29B60814581CD3B2CA3986D2683705577D45C2E7E52DC81C7A171876E5CEA74B1448BFDFAF18828EFD2519F14E45E3826634AF1949E5B535CC829A483B8A76223E5D490A257F05BDFF16F2FB22C583AB";
+// var DH_MODULUS = parseInt(DH_MODULUS_HEX, 16);
+// var DH_GENERATOR = 2;
+var OPENID_NS = "http://specs.openid.net/auth/2.0";
+var OPENID_OP_ENDPOINT = "http://localhost:3000/login";
+
+//globals
+var servers = {};
+
+function associate(options) {
+	// var modulus = options['openid.dh_modulus'] || DH_MODULUS;
+	// var generator = options['openid.dh_gen'] || DH_GENERATOR;
+
+	var assoc_handle = new Buffer(crypto.randomBytes(32)).toString('base64');
+	var dh;
+	if(options['openid.dh_modulus']) {
+		dh = crypto.createDiffieHellman(options['openid.dh_modulus'], 'base64');
+	}
+	else {
+		dh = crypto.createDiffieHellman(DH_MODULUS_HEX, 'hex');
+	}
+	var s = {
+		server: dh,
+		server_public: dh.generateKeys('base64'),
+		consumer_public: options['openid.dh_consumer_public'],
+		shared_secret: dh.computeSecret(options['openid.dh_consumer_public'], 'base64', 'base64')
+	}
+	servers[assoc_handle] = s;
+
+	var response = {}
+	response['ns'] = OPENID_NS;
+	response['assoc_handle'] = assoc_handle;
+	response['session_type'] = options['openid.session_type'];
+	response['assoc_type'] = options['openid.assoc_type'];
+	response['expires_in'] = 1209600; //in seconds
+	response['dh_server_public'] = s.server_public; //@needs btwoc
+	//create the encoded mac
+	//@needs to be generated correctly
+	var shasum = crypto.createHash('sha1');
+	shasum.update(new Buffer(s.shared_secret, 'base64').readUInt32BE(0).toString());
+	response['enc_mac_key'] = shasum.digest('base64');
+
+	//build response
+	var output = "";
+	for(var key in response) {
+		output += key+":"+response[key]+"\n";
+	}
+	return output;
+}
+
+function checkid_setup(options) {
+	var IDENTITY = "http://localhost:3000/id/chris";
+	var response = {
+		"openid.ns": OPENID_NS,
+		"openid.mode": "id_res",
+		"openid.op_endpoint": OPENID_OP_ENDPOINT,
+		"openid.claimed_id": IDENTITY,
+		"openid.identity": IDENTITY,
+		"openid.return_to": options['openid.return_to'],
+		"openid.response_nonce": new Date().toISOString()+"UNIQUE", //actually make unique
+		"openid.assoc_handle": options['openid.assoc_handle']
+	}
+
+	//sign the response
+	var openid_signed = "";
+	var kvstr = "";
+	for(var key in response) {
+		openid_signed += key.slice(7)+","; //remove "openid." from the start of the key
+		kvstr += key+":"+response[key]+"\n";
+	}
+	response['openid.signed'] = openid_signed;
+	//@needs to be generated correctly
+	response['openid.sig'] = crypto.createHmac('sha1', servers[options['openid.assoc_handle']].shared_secret)
+								.update(kvstr)
+								.digest('base64');
+
+	//convert response to url
+	var return_to = url.parse(
+		unescape(options['openid.return_to']),
+		true
+	);
+	for(var key in response) {
+		return_to.query[key] = response[key];
+	}
+	delete return_to['search'];
+	return_to = url.format(return_to);
+	return '<a href="'+return_to+'">Log into the server</a>';
+}
+
+function checkid_immediate(options) {
+	console.log(options);
+}
+
+function check_authentication(options) {
+	console.log(options);
+}
+
+function id_res(options) {
+	console.log(options);
+}
+
+var modes = {
+	"associate": associate,
+	"checkid_setup": checkid_setup,
+	"checkid_immediate": checkid_immediate,
+	"id_res": id_res,
+	"check_authentication": check_authentication
+}
+
+function openid(req, res) {
+	var options = {}
+	if(req.method == "POST") {
+		options = req.body;
+	}
+	else {
+		options = req.query;
+	}
+	return modes[options['openid.mode']](options);
+}
+
+module.exports.openid = openid;
+module.exports.modes = modes;
